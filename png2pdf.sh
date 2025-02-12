9ce87704463462eff85fce7c92f3dd731bc5b6c7
Rahul Pandare <pandare.rahul@gmail.com>
2025-02-12 17:14:09 -0600

    Updated files

--- png2pdf.sh
+++ png2pdf.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# May 21 2024, RVP
+
+# Converts all PNG files in a folder to PDF
+# Usage: ./png2pdf.sh /path/to/folder
+
 #input_folder="/media/Linux_1TB/Dropbox (City College)/CUNY/Research/Bidisperse Project/analysis/figures/system"
 #input_folder="/Users/rahul/Dropbox (City College)/CUNY/Research/Bidisperse Project/analysis/figures/phim"
 input_folder="/Users/rahul/City College Dropbox/Rahul Pandare/Sharepoint_Rahul_Surabh/png2pdf"
