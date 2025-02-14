bfb6f2ae9bc9375dd67365b5aff5b92c03215564
Rahul Pandare <pandare.rahul@gmail.com>
2025-02-13 18:06:36 -0600

    Updated files

--- gitPush.sh
+++ gitPush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull origin master --rebase  # Pulls the latest changes from GitHub to avoid conflicts
+#git pull origin master --rebase  # Pulls the latest changes from GitHub to avoid conflicts
 git add .                        # Stages all new and modified files
 git commit -m "Updated files"    # Commits the changes with a fixed message
 git push origin master           # Pushes changes to the master branch
